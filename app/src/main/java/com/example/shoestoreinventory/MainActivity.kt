99d3154f52f05bf42acaa8bc0bb0dcc51b6695ea
PackHg <pack.heng@gmail.com>
2021-01-12 21:14:11 +1100

    In activity_main.xml, replaced fragment (deprecated) by FragmentContainerView.

--- app/src/main/java/com/example/shoestoreinventory/MainActivity.kt
+++ app/src/main/java/com/example/shoestoreinventory/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.shoestoreinventory.databinding.ActivityMainBinding
@@ -17,7 +18,10 @@ class MainActivity : AppCompatActivity() {
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        val navController = this.findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                as NavHostFragment
+        val navController = navHostFragment.navController
+
         NavigationUI.setupActionBarWithNavController(this, navController)
         appBarConfiguration = AppBarConfiguration(navController.graph)
     }
